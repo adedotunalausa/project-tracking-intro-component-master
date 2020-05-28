c82566b003e5d0ccdcd3fba4c6f4e314c60f8b91
Adedotun Alausa <63458795+adedotunalausa@users.noreply.github.com>
2020-05-28 21:49:49 +0100

    Complete page with js
    Completed the desktop responsiveness and added javascript for hamburger menu in the mobile view.

---
+++ index.js
@@ -0,0 +1,6 @@
+
+function onClickMenu() {
+  document.getElementById("menu").classList.toggle("change-menu");
+  document.getElementById("menu-close").classList.toggle("change-menu");
+  document.getElementById("menu-list").classList.toggle("change-menu");
+}
